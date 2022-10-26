883dca7fc11a61a850101abb6044ce689399c716
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2022-10-26 08:43:29 -0500

    Height and width input fields already fill the original image values

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const uploadBox = document.getElementById("uploadBox"),
+    previewImg = uploadBox.querySelector("img"),
+    heightInput = document.querySelector(".height input"),
+    widthInput = document.querySelector(".width input"),
     fileInput = uploadBox.querySelector("input");
 
+const loadFile = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {//once img is loaded
+        heightInput.value = previewImg.naturalHeight;
+        widthInput.value = previewImg.naturalWidth;
+        document.querySelector(".wrapper").classList.add("active");
+    });
+    console.log(file);
+}
+
+fileInput.addEventListener("change", loadFile);
 uploadBox.addEventListener("click", () => fileInput.click());
