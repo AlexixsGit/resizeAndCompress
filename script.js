84d6083c226fdf2747926c4eeca1a39254195059
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2022-10-27 08:40:36 -0500

    Download button working

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const uploadBox = document.getElementById("uploadBox"),
     previewImg = uploadBox.querySelector("img"),
     heightInput = document.querySelector(".height input"),
     widthInput = document.querySelector(".width input"),
+    rationInput = document.querySelector(".ratio input"),
+    downloadBtn = document.querySelector(".download-btn"),
+    qualityInput = document.querySelector(".quality input"),
     fileInput = uploadBox.querySelector("input");
 
 const loadFile = (e) => {
@@ -12,10 +15,40 @@ const loadFile = (e) => {
     previewImg.addEventListener("load", () => {//once img is loaded
         heightInput.value = previewImg.naturalHeight;
         widthInput.value = previewImg.naturalWidth;
+        ogImageRatio = previewImg.naturalWidth / previewImg.naturalHeight;
         document.querySelector(".wrapper").classList.add("active");
     });
-    console.log(file);
 }
+widthInput.addEventListener("keyup", () => {
+    const height = rationInput.checked ? widthInput.value / ogImageRatio : heightInput.value;
+    heightInput.value = Math.floor(height);
+
+
+})
+
+heightInput.addEventListener("keyup", () => {
+    const width = rationInput.checked ? heightInput.value * ogImageRatio : widthInput.value;
+    widthInput.value = Math.floor(width);
+})
+
+const resizeAndDownload = () => {
+    const a = document.createElement("a");
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const imgQuality = qualityInput.checked ? 0.7 : 1.0;
+
+    canvas.width = widthInput.value;
+    canvas.height = heightInput.value;
+
+    ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+
+    a.href = canvas.toDataURL("image/jpeg", imgQuality);
+    a.download = new Date().getTime();
+    a.click();
+}
+
+downloadBtn.addEventListener("click", resizeAndDownload);
 
 fileInput.addEventListener("change", loadFile);
 uploadBox.addEventListener("click", () => fileInput.click());
